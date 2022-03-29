f722afc57aa820db64db8d9689751df58af56cb8
Rosto4eks <rosto4eks@mail.ru>
2022-03-29 22:06:20 +0300

    messages now saving

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require('express'),
     cookieParser = require('cookie-parser'),
     io = require('socket.io')(http),
     expressHbs = require('express-handlebars'),
-    hbs = require('hbs')
+    hbs = require('hbs'),
+    fs = require('fs'),
+    MessagesData = require('./data/data.json')
 
 server.use(express.static(__dirname + '/public'))
 server.set('view engine', 'hbs')
@@ -14,8 +16,16 @@ server.use('/', router)
 
 // connecting socket to chat
 io.on('connection', (socket) => {
+    for (elem in MessagesData[1]) {
+        socket.emit('chat message', {login: MessagesData[1][elem]["login"], name: MessagesData[1][elem]["name"], date: MessagesData[1][elem]["date"], time:MessagesData[1][elem]["time"] ,message: MessagesData[1][elem]["message"]});
+    }
+
     socket.on('chat message', (data) => {
-        io.emit('chat message', {login: data.login, name: data.name, message: data.message});
+        let id = MessagesData[0].nextID
+        MessagesData[1][id] = {"login": data.login,"name": data.name, "date": data.date, "time": data.time ,"message": data.message}
+        MessagesData[0]["nextID"]++
+        fs.writeFile('data/data.json', JSON.stringify(MessagesData, null, 2), ()=>{})
+        io.emit('chat message', {login: data.login, name: data.name, date: data.date, time: data.time ,message: data.message});
     })
 })
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -14,25 +14,25 @@ login = getCookie("login")
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     if (input.value) {
-        socket.emit('chat message', {login: login, name: Name + ':&nbsp', message: input.value})
+        const fulldate = new Date()
+        const date = fulldate.toLocaleDateString()
+        let hours = fulldate.getHours()
+        if (hours < 10) {hours = `0${hours}`}
+        let minutes = fulldate.getMinutes()
+        if (minutes < 10) {minutes = `0${minutes}`}
+        const time = hours + ':' + minutes
+        socket.emit('chat message', {login: login, name: Name, date: date, time: time,message: input.value})
         input.value = ''
     };
 });
 
 socket.on('chat message', (data) => {
     const item = document.createElement('li')
-    const fulldate = new Date()
-    const date = fulldate.toLocaleDateString()
-    let hours = fulldate.getHours()
-    if (hours < 10) {hours = `0${hours}`}
-    let minutes = fulldate.getMinutes()
-    if (minutes < 10) {minutes = `0${minutes}`}
-    const time = hours + ':' + minutes
     if (login === data.login) {
-        item.innerHTML = `<div class='yourBlock'><div class="message"><div class="yourName">Вы:&nbsp</div>${data.message}<div class='time'>${date}, ${time}</div></div></div>`;
+        item.innerHTML = `<div class='yourBlock'><div class="message"><div class="yourName">Вы:&nbsp</div>${data.message}<div class='time'>${data.date}, ${data.time}</div></div></div>`;
     }
     else {
-        item.innerHTML = `<div class='block'><div class="message"><div class="name">${data.name}</div>${data.message}<div class='time'>${date}, ${time}</div></div></div>`;
+        item.innerHTML = `<div class='block'><div class="message"><div class="name">${data.name}:&nbsp</div>${data.message}<div class='time'>${data.date}, ${data.time}</div></div></div>`;
     }
     messages.appendChild(item)
     messages.scrollTo(0, messages.scrollHeight)
